1b572bf2c61e327ed3eab32d4ad8b0b2b269299e
Pol Ribas <polribasrovira@gmail.com>
2019-07-09 10:46:40 +0200

    document creator

---
+++ Paginas/LoadingPage.js
@@ -0,0 +1,16 @@
+`use strict`;
+
+function LoadingPage() {
+    this.parentIE = null;
+    this.title = null;
+    this.subtitle = null;
+    this.style = null;
+}
+
+LoadingPage.prototype.render = function() {
+
+}
+
+LoadingPage.prototype.generate = function() {
+
+}
---
+++ app.js
@@ -0,0 +1,40 @@
+`use strict`;
+/*  LANDING PAGE
+        home(link home) movies (link Page)
+        title 
+        subtitle
+        footer
+
+        <<<< vamos a mostrar un loading >>>>
+        
+    MOVIES PAGE
+        home(link home) movies (link Page)
+        films of start words
+        footer
+*/
+
+function main() {
+    var entryPoint = '/';
+
+    function generateLayout() {
+        generatrHeader();
+
+        generateFooter();
+        activateRouter();
+    }
+
+    function generatrHeader() {
+
+    }
+
+    function generateFooter() {
+
+    }
+
+    function activateRouter() {
+
+    }
+
+    generateLayout();
+}
+window.addEventListener('load', main);
